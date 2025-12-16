782207cb4eaa18a4625929eff5199b7cf2cc8bde
chase-urasaki <chasemu@hawaii.edu>
2025-12-15 16:43:58 -1000

    added data locally and fits-to-png converter

---
+++ fits-to-png.py
@@ -0,0 +1,42 @@
+#%% 
+import numpy as np 
+from astropy.io import fits
+
+# Code to convert fits images into pngs 
+from astropy.visualization import ZScaleInterval, ImageNormalize
+import matplotlib.pyplot as plt
+import os
+
+#%%
+def fits_to_png(fits_file, output_png):
+    # Open the FITS file
+    with fits.open(fits_file) as hdul:
+        image_data = hdul[0].data
+
+    # Normalize the image data using ZScale
+    norm = ImageNormalize(image_data, interval=ZScaleInterval())
+
+    # Create a figure and axis
+    plt.figure(figsize=(8, 8))
+    plt.imshow(image_data, cmap='gray', norm=norm)
+    plt.axis('off')  # Hide axes
+
+    # Save the figure as a PNG file
+    plt.savefig(output_png, bbox_inches='tight', pad_inches=0)
+    plt.close()
+#%%
+if __name__ == "__main__":
+    # input directory 
+    input_dir = "./data/251204/fits"
+    output_dir = "./data/251204/pngs"
+ 
+    os.makedirs(output_dir, exist_ok=True)
+    fits_files = [f for f in os.listdir(input_dir) if f.endswith('.fits')]
+    for fits_file in fits_files:
+        fits_path = os.path.join(input_dir, fits_file)
+        png_filename = fits_file.replace('.fits', '.png')
+        output_png_path = os.path.join(output_dir, png_filename)
+        fits_to_png(fits_path, output_png_path)
+        print(f"Converted {fits_file} to {png_filename}")
+
+# %%
