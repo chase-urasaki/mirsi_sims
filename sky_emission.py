bc394c350e94c522b6def07f0ed426f0a028ea94
chase-urasaki <chasemu@hawaii.edu>
2026-03-16 15:26:39 -1000

    updated sky emission code

--- sky_emission.py
+++ sky_emission.py
@@ -4,7 +4,79 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-wl_um, E = np.genfromtxt('mk_skybg_nq_10_10_ph.dat', unpack=True)
+wl_nm, E = np.genfromtxt('mk_skybg_nq_10_10_ph.dat', unpack=True)
+# Data has units of ph/sec/as^2/nm/m^2, and wavelength in nm. Convert to microns and check units carefully!
+
+#%% print the first few values to check
+print("Wavelength (um):", wl_nm[:5])
+print("Sky emission (ph/s/m^2/arcsec^2/nm):", E[:5])
+
+#%% 
+# Plot the sky emission spectrum
+plt.figure(figsize=(10, 6))
+plt.plot(wl_nm, E, label='Sky Emission Spectrum')
+#plt.xlim(8, 14)
+plt.ylim(0, np.max(E)*1.1)
+plt.xlabel('Wavelength [nm]')
+plt.ylabel('Sky Emission (ph/s/m^2/arcsec^2/nm)')
+plt.title('Sky Emission Spectrum (7-14 um)')
+plt.grid()
+#%%
+# load the filter profile from the .dat file 
+filter_wl, filter_trans = np.genfromtxt('IRTF_MIRSI.N.dat', unpack=True)
+#%% 
+# Convert filter wl from angstroms to nm 
+filter_wl_nm = filter_wl / 10.0  # angstroms to nm
+#%% 
+# Use the twin y axis to plot the filter transmission on the same plot
+fig, ax1 = plt.subplots(figsize=(10, 6))
+ax1.plot(wl_nm, E, label='Sky Emission Spectrum', color='blue')
+ax1.set_xlabel('Wavelength [nm]')
+ax1.set_ylabel('Sky Emission (ph/s/m^2/arcsec^2/nm)', color='blue')
+ax1.tick_params(axis='y', labelcolor='blue')
+ax2 = ax1.twinx()
+ax2.plot(filter_wl_nm, filter_trans, label='Filter Transmission', color='orange')
+ax2.set_ylabel('Filter Transmission', color='orange')
+ax2.tick_params(axis='y', labelcolor='orange')
+plt.title('Sky Emission Spectrum and Filter Transmission')
+plt.grid()
+plt.show()  
+
+
+#%% 
+# Interpolate the filter transmission onto the sky emission wavelength grid
+from scipy.interpolate import interp1d
+filter_interp = interp1d(filter_wl_nm, filter_trans, bounds_error=False, fill_value=0.0)
+filter_on_sky = filter_interp(wl_nm)
+#%%
+# Apply the filter transmission to the sky emission spectrum
+E_filtered = E * filter_on_sky  
+
+plt.figure(figsize=(10, 6))
+plt.plot(wl_nm, E, label='Sky Emission Spectrum', color='blue')
+plt.plot(wl_nm, E_filtered, label='Filtered Sky Emission', color='red')
+plt.xlabel('Wavelength [nm]')
+plt.ylabel('Sky Emission (ph/s/m^2/arcsec^2/nm)')
+plt.title('Sky Emission Spectrum Before and After Filter')
+plt.legend()
+plt.grid()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
 wl_um /= 1000.0  # if your file wavelength is in nm; double-check!
 
 # Filter edges (given)
@@ -14,7 +86,7 @@ lam1, lam2 = 8.5, 13.0
 T = 0.65
 
 # Effective collecting area (geometric for now)
-D = 3.0  # m
+D = 3.2  # m
 A_geo = np.pi * (D/2)**2
 
 # Pixel solid angle in arcsec^2/pixel (MIRSI)
